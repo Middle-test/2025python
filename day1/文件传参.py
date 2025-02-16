4b4f5ef7a597d04b8c66e813e9333eec6b504d45
Azumate <1620623344@qq.com>
2025-02-16 20:14:06 +0800

    文件传参读取测试

---
+++ day1/文件传参.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def write_hello(file_path):
+    file = open(file_path, 'w', encoding='utf-8')
+    file.write('hello')
+    file.close()
+
+
+if __name__ == '__main__':
+    write_hello(sys.argv[1])
